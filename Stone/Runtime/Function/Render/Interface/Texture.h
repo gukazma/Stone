760cbaa74940e06dbf2438241dc9f8a142baad0b
xhguleixin123 <18017158729@163.com>
2022-05-28 10:47:09 +0800

    ✨ feat: add texturepool

--- Stone/Runtime/Function/Render/Interface/Texture.h
+++ Stone/Runtime/Function/Render/Interface/Texture.h
@@ -1,6 +1,8 @@
 #pragma once
-#include<string>
-#include<memory>
+#include <string>
+#include <memory>
+#include <map>
+#include <Core/Base/PublicSingleton.h>
 namespace Stone
 {
     class Texture
@@ -17,5 +19,20 @@ namespace Stone
     public:
         static std::shared_ptr<Texture2D> create(uint32_t width, uint32_t height);
         static std::shared_ptr<Texture2D> create(const std::string& path);
+    };
+
+    class TexturePool : public PublicSingleton<TexturePool>
+    {
+    public:
+        std::shared_ptr<Texture2D> getTexture(const std::string& path)
+        {
+            if (m_Map.find(path)==m_Map.end())
+            {
+                m_Map[path] = Texture2D::create(path);
+            }
+            return m_Map[path];
+        }
+    private:
+        std::map <std::string, std::shared_ptr<Texture2D>> m_Map;
     };
 }
